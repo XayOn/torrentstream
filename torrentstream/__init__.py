d85d54e3ac3a109cae7320f9443310afc9a1ad13
David Francos Cuartero <dfrancos@buguroo.com>
2016-04-25 22:47:53 +0200

    Small refactor

--- torrentstream/__init__.py
+++ torrentstream/__init__.py
@@ -17,45 +17,60 @@ __email__ = 'dfrancos@buguroo.com'
 __version__ = '0.1.0'
 
 
-async def _stream_torrent(loop, torrent, stream_func, filter_func):
+TIMEOUT_CACHE_FILL = 5 * 60
+TIMEOUT_START = 10 * 60
+PERCENT_CACHE = 5
+
+
+async def stream_torrent(loop, torrent, stream_func, filter_func):
     """
-        Stream torrent
+        Stream torrent.
+        This will launch:
+            - A logger to log torrent alerts (while torrent is downloading)
+            - Torrent download
+            - Once torrent.started, will try to find a playable file, and
+              fill a ``PERCENT_CACHE`` cache to start playing
+
     """
+
     async def alert_watcher(torrent):
         """ Watch and log torrent alerts from LT """
-        while True:
+        while not torrent.finished:
             alert = torrent.session.pop_alert()
             if alert:
                 LOG.info(alert)
             await asyncio.sleep(1)
 
+    # Force sequential mode
+    torrent.sequential(True)
+
+    # Start alert watcher task
     loop.create_task(alert_watcher(torrent))
 
+    # If we're actually re-asking for a torrent, just relaunch streaming
     if torrent.finished:
         return [wait_for_completion(torrent),
                 stream_func(loop, filter_func(torrent.files))]
 
-    with asyncio.timeout(10 * 60):  #: TODO Make this timeout configurable
+    # Otherwise, wait for torrent to start or finish in TIMEOUT_START+
+    # seconds
+    with asyncio.timeout(TIMEOUT_START):
         while not torrent.started and not torrent.finished:
             await asyncio.sleep(5)
 
-    self.sequential(True)
-
+    # Only download filtered file
     playable_tfile = torrent.download_only(await filter_func(torrent.files))
-    LOG.info("Found playable file: %s", playable_tfile)
 
+    # If filter matches nothing, stop
     if not playable_tfile:
         raise Exception("Could not find a playable source that matches"
                         " filter function")
 
-    try:
-        with asyncio.timeout(5 * 60):
-            while True:
-                if playable_tfile.completed_percent >= 5:
-                    break
-                await asyncio.sleep(5)
-    except asyncio.TimeoutError:
-        raise Exception('Could not get playable source in time')
+    # Try to fill the cache, if it's not filled within the first
+    # ``TIMEOUT_CACHE_FILL`` seconds, exit
+    with asyncio.timeout(TIMEOUT_CACHE_FILL):
+        while playable_tfile.completed_percent < PERCENT_CACHE:
+            await asyncio.sleep(5)
 
     return await asyncio.gather([wait_for_completion(torrent),
             stream_func(loop, playable_tfile)]
