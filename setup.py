1e65b6431544ef21705842a87fc5ef691efff697
David Francos Cuartero <dfrancos@buguroo.com>
2016-06-30 01:33:12 +0200

    Unnecesary stuff gone

--- setup.py
+++ setup.py
@@ -1,58 +1,11 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""
+    Torrentstream
+"""
 
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = [
-    # TODO: put package requirements here
-]
-
-test_requirements = [
-    # TODO: put package test requirements here
-]
+from setuptools import setup
 
 setup(
-    name='torrentstream',
-    version='0.1.1',
-    description="Python3.5 simple BitTorrent streaming library based on libtorrent",
-    long_description=readme + '\n\n' + history,
-    author="David Francos Cuartero",
-    author_email='dfrancos@buguroo.com',
-    url='https://github.com/XayOn/torrentstream',
-    packages=[
-        'torrentstream',
-    ],
-    package_dir={'torrentstream':
-                 'torrentstream'},
-    include_package_data=True,
-    install_requires=requirements,
-    license="ISCL",
-    zip_safe=False,
-    keywords='torrentstream',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
+    setup_requires=['pbr', 'setuptools'],
+    pbr=True,
 )
